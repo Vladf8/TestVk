c2993629a40bdb9050807faa33232b6b5eb0f2d7
Vladf8 <fishenko.vlad@gmail.com>
2020-04-08 13:18:34 +0300

    get photo and friends

--- vklogin/views.py
+++ vklogin/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from allauth.socialaccount.models import SocialAccount
 import requests
 
-def getUserFriends(id):
-    print('test')
+def getUserFriends(token:str):
+    resp=requests.get('https://api.vk.com/method/friends.get?v=5.52&access_token={}&count=1000000'.format(token))
+    friends=resp.json()
+
+    return friends.get('count')
 
 def index(request):
     if request.user.is_authenticated:
@@ -20,9 +23,19 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def getUserInfo(token:str):
+    resp= requests.get('https://api.vk.com/method/users.get?v=5.52&access_token={}&fields=photo_max&name_case=nom'.format(token))
+    userInfo=resp.json()
+    return  userInfo
+
 def callback(request):
     code=request.GET.get('code')
     resp=requests.get('https://oauth.vk.com/access_token?client_id=7396296&client_secret=hcSFV9SDvpLw19NdiwrX&redirect_uri=https://vladtestvk.herokuapp.com/callback&code={}'.format(code))
     token=resp.json()
     context={"user_token":token.get('access_token'),"user_id":token.get('user_id'),"is_authenticated":True}
+    if context.get('user_token') != None:
+        userInfo= getUserInfo(context['user_token'])
+        context['username']=userInfo.get('first_name')+' '+userInfo.get('last_name')
+        context['photo']=userInfo.get('photo_max')
+        context['friends_count']=getUserFriends(context['user_token'])
     return render(request,'index.html',context)
